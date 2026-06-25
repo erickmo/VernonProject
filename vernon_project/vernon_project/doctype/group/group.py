233d9b1ccd4c93e036df96fe0326e497fca97795
erickmo <mo@vernon.id>
2026-06-25 11:06:20 +0000

    feat(groups): add stable level_id to Group Level and Project Todo

--- vernon_project/vernon_project/doctype/group/group.py
+++ vernon_project/vernon_project/doctype/group/group.py
@@ -1,8 +1,12 @@
 # Copyright (c) 2026, Vernon and contributors
 # For license information, please see license.txt
 
+import frappe
 from frappe.model.document import Document
 
 
 class Group(Document):
-	pass
+	def validate(self):
+		for row in self.levels:
+			if not row.level_id:
+				row.level_id = frappe.generate_hash(length=10)
