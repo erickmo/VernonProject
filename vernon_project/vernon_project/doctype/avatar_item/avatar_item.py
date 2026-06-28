56e9c5d787dd2b4c6b7eb0492cef92fb17bca4ba
erickmo <mo@vernon.id>
2026-06-28 11:29:18 +0000

    feat(avatar): Avatar Item = (style, slot, option_value) for DiceBear

--- vernon_project/vernon_project/doctype/avatar_item/avatar_item.py
+++ vernon_project/vernon_project/doctype/avatar_item/avatar_item.py
@@ -1,13 +1,7 @@
 # Copyright (c) 2026, Vernon and contributors
-# For license information, please see license.txt
 
-import frappe
 from frappe.model.document import Document
 
 
 class AvatarItem(Document):
-	def validate(self):
-		if self.slot in ("Hat", "Face") and not self.socket:
-			frappe.throw("Socket is required for Hat/Face items")
-		if self.slot == "Base":
-			self.socket = None
+	pass
